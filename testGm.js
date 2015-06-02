075d5ce33f7478ce1b51d91840d48dde04206839
lei-clearsky <sportzhulei@gmail.com>
2015-06-02 16:20:34 -0400

    tested spookyJS not functioning correctly, tested start() without promise

--- testGm.js
+++ testGm.js
@@ -1,6 +1,62 @@
 var phantom = require('phantom');
 var gm = require('gm');
 var q = require('q');
+var Spooky = require('spooky');
+// var casper = require('casper');
+
+// test spooky
+// var spooky = new Spooky({
+//         child: {
+//             transport: 'http'
+//         },
+//         casper: {
+//             logLevel: 'debug',
+//             verbose: true
+//         }
+//     }, function (err) {
+//         if (err) {
+//             e = new Error('Failed to initialize SpookyJS');
+//             e.details = err;
+//             throw e;
+//         }
+//         spooky.start('https://news.ycombinator.com/news?p=1');
+        
+//         spooky.then(function () {
+//         	console.log('test casper in spooky!!!!!');
+//             this.emit('hello', 'Hello, from ' + this.evaluate(function () {
+//                 return document.title;
+//             }));
+//         });
+//         spooky.run();
+//     });
+
+// spooky.on('error', function (e, stack) {
+// 	console.log('errors?????');
+//     console.error(e);
+
+//     if (stack) {
+//         console.log(stack);
+//     }
+// });
+
+// // Uncomment this block to see all of the things Casper has to say.
+// // There are a lot.
+// // He has opinions.
+// spooky.on('console', function (line) {
+//     console.log(line);
+// });
+
+// spooky.on('hello', function (greeting) {
+//     console.log(greeting);
+// });
+
+// spooky.on('log', function (log) {
+//     if (log.space === 'remote') {
+//         console.log(log.message.replace(/ \- .*/, ''));
+//     }
+// });
+// end test
+
 
 var url1 = "https://news.ycombinator.com/news?p=1";
 var url2 = "https://news.ycombinator.com/news?p=2";
@@ -8,11 +64,11 @@ var url2 = "https://news.ycombinator.com/news?p=2";
 // var url1 = "http://lei-clearsky.github.io/nanodegree-fewd-p2/";
 // var url2 = "http://lei-clearsky.github.io/lei-resume/";
 
-var img1path = 'img5.png';
-var img2path = 'img6.png';
+var img1path = 'img7.png';
+var img2path = 'img8.png';
 
 function getSnapshot(url, img) {
-    var df = q.defer();
+    // var df = q.defer();
 
     phantom.create(function(ph) {
         ph.createPage(function(page) {
@@ -22,12 +78,12 @@ function getSnapshot(url, img) {
                   page.render(img);
                   ph.exit();
 
-                  df.resolve(img);
+                  // df.resolve(img);
               });
         });
     });
 
-    return df.promise;
+    // return df.promise;
 };
 
 function createDiff(img1, img2) {
@@ -46,23 +102,34 @@ function createDiff(img1, img2) {
       });
 };
 
+// function start() {
+//     var promises = [];
+
+//     promises.push(getSnapshot(url1, img1path));
+//     promises.push(getSnapshot(url2, img2path));
+
+//       q.all(promises).then(function(results) {
+
+//           console.log("Comparing " + results[0] + " and " + results[1] + "....");
+//           // Eventhough I used promises to make sure createDiff() is not called until page.render() has been called,
+//           // I was not able to find a way to make sure phantomjs was finished writing screenshots before gm.compare() was called
+//           // Need better solution
+//           setTimeout(function() { 
+//             createDiff(results[0], results[1]);
+//           }, 1000);
+
+//       });
+// }
+
 function start() {
-    var promises = [];
 
-    promises.push(getSnapshot(url1, img1path));
-    promises.push(getSnapshot(url2, img2path));
-
-      q.all(promises).then(function(results) {
-
-          console.log("Comparing " + results[0] + " and " + results[1] + "....");
-          // Eventhough I used promises to make sure createDiff() is not called until page.render() has been called,
-          // I was not able to find a way to make sure phantomjs was finished writing screenshots before gm.compare() was called
-          // Need better solution
-          setTimeout(function() { 
-            createDiff(results[0], results[1]);
-          }, 1000);
-
-      });
+	getSnapshot(url1, img1path);
+	getSnapshot(url2, img2path);
+	
+	setTimeout(function() { 
+        createDiff(img1path, img2path);
+    }, 3000);
+    
 }
 
 start();
