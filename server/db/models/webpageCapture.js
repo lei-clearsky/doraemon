7eb2a88df8439137253cf734e832475b559ee0cd
Victor Atteh <victoratteh@gmail.com>
2015-06-05 21:05:09 -0400

    changed new Date() to Date.now

--- server/db/models/webpageCapture.js
+++ server/db/models/webpageCapture.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var schema = new mongoose.Schema({
     captureTime: { 
         type: Date, 
-        default: new Date()
+        default: Date.now
     },
     websiteUrl: {
         type: String
